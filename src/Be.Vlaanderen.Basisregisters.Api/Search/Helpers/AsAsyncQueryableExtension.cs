bd7b2411b6c92e40544fd84c346b790e363580ed
David Cumps <david@cumps.be>
2019-04-24 19:48:01 +0200

    refactor: bump to .net core 2.2.4

--- src/Be.Vlaanderen.Basisregisters.Api/Search/Helpers/AsAsyncQueryableExtension.cs
+++ src/Be.Vlaanderen.Basisregisters.Api/Search/Helpers/AsAsyncQueryableExtension.cs
@@ -12,18 +12,14 @@ namespace Be.Vlaanderen.Basisregisters.Api.Search.Helpers
     public static class AsAsyncQueryableExtension
     {
         public static IQueryable<T> AsAsyncQueryable<T>(this IEnumerable<T> source)
-        {
-            return source is IQueryable<T> queryable
+            => source is IQueryable<T> queryable
                 ? queryable.AsAsyncQueryable()
                 : new AsyncQueryable<T>(source);
-        }
 
         public static IQueryable<T> AsAsyncQueryable<T>(this IQueryable<T> source)
-        {
-            return source is IAsyncEnumerable<T>
+            => source is IAsyncEnumerable<T>
                 ? source
                 : new AsyncQueryable<T>(source);
-        }
     }
 
     internal class AsyncQueryable<T> : IQueryable<T>, IAsyncEnumerable<T>
@@ -36,17 +32,23 @@ namespace Be.Vlaanderen.Basisregisters.Api.Search.Helpers
         public AsyncQueryable(Expression expression)
             => _queryable = new EnumerableQuery<T>(expression);
 
-        public Type ElementType => _queryable.ElementType;
+        public Type ElementType
+            => _queryable.ElementType;
 
-        public Expression Expression => _queryable.Expression;
+        public Expression Expression
+            => _queryable.Expression;
 
-        IQueryProvider IQueryable.Provider => new AsyncQueryProvider<T>(_queryable.Provider);
+        IQueryProvider IQueryable.Provider
+            => new AsyncQueryProvider<T>(_queryable.Provider);
 
-        IEnumerator IEnumerable.GetEnumerator() => _queryable.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator()
+            => _queryable.GetEnumerator();
 
-        IEnumerator<T> IEnumerable<T>.GetEnumerator() => _queryable.GetEnumerator();
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+            => _queryable.GetEnumerator();
 
-        public IAsyncEnumerator<T> GetEnumerator() => new AsyncEnumerator<T>(_queryable);
+        public IAsyncEnumerator<T> GetEnumerator()
+            => new AsyncEnumerator<T>(_queryable);
     }
 
     internal class AsyncQueryProvider<TEntity> : IAsyncQueryProvider
@@ -54,56 +56,44 @@ namespace Be.Vlaanderen.Basisregisters.Api.Search.Helpers
         private readonly IQueryProvider _inner;
 
         internal AsyncQueryProvider(IQueryProvider inner)
-        {
-            _inner = inner;
-        }
+            => _inner = inner;
 
         public IQueryable CreateQuery(Expression expression)
-        {
-            return new AsyncQueryable<TEntity>(expression);
-        }
+            => new AsyncQueryable<TEntity>(expression);
 
         public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
-        {
-            return new AsyncQueryable<TElement>(expression);
-        }
+            => new AsyncQueryable<TElement>(expression);
 
         public object Execute(Expression expression)
-        {
-            return _inner.Execute(expression);
-        }
+            => _inner.Execute(expression);
 
         public TResult Execute<TResult>(Expression expression)
-        {
-            return _inner.Execute<TResult>(expression);
-        }
+            => _inner.Execute<TResult>(expression);
 
         public IAsyncEnumerable<TResult> ExecuteAsync<TResult>(Expression expression)
-        {
-            return new AsyncQueryable<TResult>(expression);
-        }
+            => new AsyncQueryable<TResult>(expression);
 
         public Task<TResult> ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
-        {
-            return Task.FromResult(Execute<TResult>(expression));
-        }
+            => Task.FromResult(Execute<TResult>(expression));
     }
-    
+
     internal class AsyncEnumerator<T> : IAsyncEnumerator<T>
     {
         private readonly IEnumerator<T> _enumerator;
 
         public AsyncEnumerator(IEnumerable<T> enumerable)
-            : this(enumerable.GetEnumerator())
-        { }
+            : this(enumerable.GetEnumerator()) { }
 
         public AsyncEnumerator(IEnumerator<T> enumerator)
             => _enumerator = enumerator;
 
-        public T Current => _enumerator.Current;
+        public T Current
+            => _enumerator.Current;
 
-        public Task<bool> MoveNext(CancellationToken cancellationToken) => Task.FromResult(_enumerator.MoveNext());
+        public Task<bool> MoveNext(CancellationToken cancellationToken)
+            => Task.FromResult(_enumerator.MoveNext());
 
-        public void Dispose() => _enumerator.Dispose();
+        public void Dispose()
+            => _enumerator.Dispose();
     }
 }

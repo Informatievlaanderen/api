97886139a3239ef618ff0d23e9eb7e3a937e42da
Wouter De Rijck <wouter.derijck@kb.vlaanderen.be>
2019-04-24 11:03:20 +0200

    fix: change the PagedQueryable.Items type
    Changed the Items type to IAsyncQueryable
    which explictly implementsIAsyncEnumerable<T>
    to avoid exceptions when ToListAsync is called on the
    IQueryable<T>

--- src/Be.Vlaanderen.Basisregisters.Api/Search/Pagination/PagedQueryable.cs
+++ src/Be.Vlaanderen.Basisregisters.Api/Search/Pagination/PagedQueryable.cs
@@ -1,17 +1,18 @@
 namespace Be.Vlaanderen.Basisregisters.Api.Search.Pagination
 {
     using System.Linq;
+    using Helpers;
     using Sorting;
 
     public class PagedQueryable<T>
     {
-        public IQueryable<T> Items { get; }
+        public IAsyncQueryable<T> Items { get; }
         public PaginationInfo PaginationInfo { get; }
         public SortingHeader Sorting { get; }
 
         public PagedQueryable(IQueryable<T> items, PaginationInfo paginationInfo, SortingHeader sortingHeader)
         {
-            Items = items;
+            Items = items.AsAsyncQueryable();
             PaginationInfo = paginationInfo;
             Sorting = sortingHeader;
         }

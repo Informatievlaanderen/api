5a0ab18fd931445787a59e2d98c2078181a4c07e
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2020-06-30 19:51:30 +0200

    feat: add atomfeedwriter write metadata with alternate uris

--- src/Be.Vlaanderen.Basisregisters.Api/Syndication/AtomFeedWriterExtensions.cs
+++ src/Be.Vlaanderen.Basisregisters.Api/Syndication/AtomFeedWriterExtensions.cs
@@ -3,6 +3,8 @@ namespace Be.Vlaanderen.Basisregisters.Api.Syndication
     using Microsoft.SyndicationFeed;
     using Microsoft.SyndicationFeed.Atom;
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
 
     public static class AtomFeedWriterExtensions
@@ -17,6 +19,7 @@ namespace Be.Vlaanderen.Basisregisters.Api.Syndication
         /// <param name="selfUri">the self referencing uri of the feed</param>
         /// <param name="relatedUrls">optional related urls</param>
         /// <returns></returns>
+        [Obsolete("Use the extension method with the AtomFeedConfiguration parameter.")]
         public static async Task WriteDefaultMetadata(
             this AtomFeedWriter atomFeedWriter,
             string id,
@@ -25,17 +28,47 @@ namespace Be.Vlaanderen.Basisregisters.Api.Syndication
             Uri selfUri,
             params string[] relatedUrls)
         {
-            await atomFeedWriter.WriteId(id);
-            await atomFeedWriter.WriteTitle(title);
-            await atomFeedWriter.WriteSubtitle("Basisregisters Vlaanderen stelt u in staat om alles te weten te komen rond: de Belgische gemeenten; de Belgische postcodes; de Vlaamse straatnamen; de Vlaamse adressen; de Vlaamse gebouwen en gebouweenheden; de Vlaamse percelen; de Vlaamse organisaties en organen; de Vlaamse dienstverlening.");
-            await atomFeedWriter.WriteGenerator("Basisregisters Vlaanderen", "https://basisregisters.vlaanderen.be", version);
-            await atomFeedWriter.WriteRights($"Copyright (c) 2017-{DateTime.Now.Year}, Informatie Vlaanderen");
-            await atomFeedWriter.WriteUpdated(DateTimeOffset.UtcNow);
-            await atomFeedWriter.Write(new SyndicationPerson("agentschap Informatie Vlaanderen", "informatie.vlaanderen@vlaanderen.be", AtomContributorTypes.Author));
-            await atomFeedWriter.Write(new SyndicationLink(selfUri, AtomLinkTypes.Self));
+            await atomFeedWriter.WriteDefaultMetadata(
+                new AtomFeedConfiguration(
+                    id,
+                    title,
+                    "Basisregisters Vlaanderen stelt u in staat om alles te weten te komen rond: de Belgische gemeenten; de Belgische postcodes; de Vlaamse straatnamen; de Vlaamse adressen; de Vlaamse gebouwen en gebouweenheden; de Vlaamse percelen; de Vlaamse organisaties en organen; de Vlaamse dienstverlening.",
+                    "Basisregisters Vlaanderen",
+                    "https://basisregisters.vlaanderen.be",
+                    version,
+                    $"Copyright (c) 2017-{DateTime.Now.Year}, Informatie Vlaanderen",
+                    DateTimeOffset.Now,
+                    new SyndicationPerson("agentschap Informatie Vlaanderen", "informatie.vlaanderen@vlaanderen.be",
+                        AtomContributorTypes.Author),
+                    new SyndicationLink(selfUri, AtomLinkTypes.Self),
+                    new List<SyndicationLink>(),
+                    relatedUrls.Select(x => new SyndicationLink(new Uri(x), AtomLinkTypes.Related)).ToList()));
+        }
 
-            foreach (var relatedUrl in relatedUrls)
-                await atomFeedWriter.Write(new SyndicationLink(new Uri(relatedUrl), AtomLinkTypes.Related));
+        /// <summary>
+        /// Write default meta data for an atom feed
+        /// </summary>
+        /// <param name="atomFeedWriter">the AtomFeedWriter</param>
+        /// <param name="atomFeedConfiguration">the configuration of the atom feed</param>
+        /// <returns></returns>
+        public static async Task WriteDefaultMetadata(
+            this AtomFeedWriter atomFeedWriter,
+            AtomFeedConfiguration atomFeedConfiguration)
+        {
+            await atomFeedWriter.WriteId(atomFeedConfiguration.Id);
+            await atomFeedWriter.WriteTitle(atomFeedConfiguration.Title);
+            await atomFeedWriter.WriteSubtitle(atomFeedConfiguration.Subtitle);
+            await atomFeedWriter.WriteGenerator(atomFeedConfiguration.GeneratorTitle, atomFeedConfiguration.GeneratorUri, atomFeedConfiguration.GeneratorVersion);
+            await atomFeedWriter.WriteRights(atomFeedConfiguration.Rights);
+            await atomFeedWriter.WriteUpdated(atomFeedConfiguration.Updated);
+            await atomFeedWriter.Write(atomFeedConfiguration.Author);
+            await atomFeedWriter.Write(atomFeedConfiguration.SelfUri);
+
+            foreach (var alternateUri in atomFeedConfiguration.AlternateUris)
+                await atomFeedWriter.Write(alternateUri);
+
+            foreach (var relatedUri in atomFeedConfiguration.RelatedUris)
+                await atomFeedWriter.Write(relatedUri);
         }
     }
 }

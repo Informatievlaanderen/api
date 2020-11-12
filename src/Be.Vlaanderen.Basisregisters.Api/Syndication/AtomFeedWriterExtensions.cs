26ab7bafe0748f08ff47872ed65ae1077b8a581c
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-11-12 15:04:14 +0100

    fix: removed deprecated function
    BREAKING CHANGES

--- src/Be.Vlaanderen.Basisregisters.Api/Syndication/AtomFeedWriterExtensions.cs
+++ src/Be.Vlaanderen.Basisregisters.Api/Syndication/AtomFeedWriterExtensions.cs
@@ -13,42 +13,6 @@ namespace Be.Vlaanderen.Basisregisters.Api.Syndication
         /// Write default meta data for an atom feed
         /// </summary>
         /// <param name="atomFeedWriter">the AtomFeedWriter</param>
-        /// <param name="id">the id of the feed</param>
-        /// <param name="title">the title of the feed</param>
-        /// <param name="version">the version of the feed</param>
-        /// <param name="selfUri">the self referencing uri of the feed</param>
-        /// <param name="relatedUrls">optional related urls</param>
-        /// <returns></returns>
-        [Obsolete("Use the extension method with the AtomFeedConfiguration parameter.")]
-        public static async Task WriteDefaultMetadata(
-            this AtomFeedWriter atomFeedWriter,
-            string id,
-            string title,
-            string version,
-            Uri selfUri,
-            params string[] relatedUrls)
-        {
-            await atomFeedWriter.WriteDefaultMetadata(
-                new AtomFeedConfiguration(
-                    id,
-                    title,
-                    "Basisregisters Vlaanderen stelt u in staat om alles te weten te komen rond: de Belgische gemeenten; de Belgische postcodes; de Vlaamse straatnamen; de Vlaamse adressen; de Vlaamse gebouwen en gebouweenheden; de Vlaamse percelen; de Vlaamse organisaties en organen; de Vlaamse dienstverlening.",
-                    "Basisregisters Vlaanderen",
-                    "https://basisregisters.vlaanderen.be",
-                    version,
-                    $"Copyright (c) 2017-{DateTime.Now.Year}, Informatie Vlaanderen",
-                    DateTimeOffset.Now,
-                    new SyndicationPerson("agentschap Informatie Vlaanderen", "informatie.vlaanderen@vlaanderen.be",
-                        AtomContributorTypes.Author),
-                    new SyndicationLink(selfUri, AtomLinkTypes.Self),
-                    new List<SyndicationLink>(),
-                    relatedUrls.Select(x => new SyndicationLink(new Uri(x), AtomLinkTypes.Related)).ToList()));
-        }
-
-        /// <summary>
-        /// Write default meta data for an atom feed
-        /// </summary>
-        /// <param name="atomFeedWriter">the AtomFeedWriter</param>
         /// <param name="atomFeedConfiguration">the configuration of the atom feed</param>
         /// <returns></returns>
         public static async Task WriteDefaultMetadata(

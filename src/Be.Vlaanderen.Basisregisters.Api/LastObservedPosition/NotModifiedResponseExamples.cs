823520d3a5abb649b125d7cb9188b4fe3cc88729
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2020-11-16 09:40:17 +0100

    fix: add NotModified response to LastObeservedPosition

---
+++ src/Be.Vlaanderen.Basisregisters.Api/LastObservedPosition/NotModifiedResponseExamples.cs
@@ -0,0 +1,11 @@
+namespace Be.Vlaanderen.Basisregisters.Api.LastObservedPosition
+{
+    using Swashbuckle.AspNetCore.Filters;
+
+    public class NotModifiedResponseExamples : IExamplesProvider<object>
+    {
+        // https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.5
+        // The 304 response MUST NOT contain a message-body
+        public object GetExamples() => null;
+    }
+}

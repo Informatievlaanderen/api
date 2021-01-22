18578a2729e0091a1f5e57fa9ed1af478885ac1d
Wouter De Rijck <wouter.derijck@vlaanderen.be>
2021-01-22 11:44:43 +0100

    fix: always assign status code 500 as a default for ApiExceptions

--- src/Be.Vlaanderen.Basisregisters.Api/Exceptions/ApiException.cs
+++ src/Be.Vlaanderen.Basisregisters.Api/Exceptions/ApiException.cs
@@ -5,15 +5,15 @@ namespace Be.Vlaanderen.Basisregisters.Api.Exceptions
 
     public class ApiException : Exception
     {
-        public int StatusCode { get; }
+        public int StatusCode { get; } = StatusCodes.Status500InternalServerError;
 
-        public ApiException() => StatusCode = StatusCodes.Status500InternalServerError;
+        public ApiException() { }
 
-        public ApiException(string message) : base(message) => StatusCode = StatusCodes.Status500InternalServerError;
-
-        public ApiException(string message, int statusCode) : base(message) => StatusCode = statusCode;
+        public ApiException(string message) : base(message)  { }
 
         public ApiException(string message, Exception inner) : base(message, inner) { }
+
+        public ApiException(string message, int statusCode) : base(message) => StatusCode = statusCode;
 
         public ApiException(string message, int statusCode, Exception inner) : base(message, inner) => StatusCode = statusCode;
     }

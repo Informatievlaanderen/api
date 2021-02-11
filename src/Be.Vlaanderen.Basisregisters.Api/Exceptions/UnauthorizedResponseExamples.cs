bfb74ed4f23b1d425d51d805f24576d45d4038e3
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2021-02-11 13:19:09 +0100

    feat: add unauthorized example

---
+++ src/Be.Vlaanderen.Basisregisters.Api/Exceptions/UnauthorizedResponseExamples.cs
@@ -0,0 +1,30 @@
+namespace Be.Vlaanderen.Basisregisters.Api.Exceptions
+{
+    using BasicApiProblem;
+    using Microsoft.AspNetCore.Http;
+    using Swashbuckle.AspNetCore.Filters;
+
+    public class UnauthorizedResponseExamples : IExamplesProvider<ProblemDetails>
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ProblemDetailsHelper _problemDetailsHelper;
+
+        public UnauthorizedResponseExamples(
+            IHttpContextAccessor httpContextAccessor,
+            ProblemDetailsHelper problemDetailsHelper)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _problemDetailsHelper = problemDetailsHelper;
+        }
+
+        public ProblemDetails GetExamples() =>
+            new ProblemDetails
+            {
+                ProblemTypeUri = "urn:be.vlaanderen.basisregisters.api:unauthorized",
+                HttpStatus = StatusCodes.Status401Unauthorized,
+                Title = ProblemDetails.DefaultTitle,
+                Detail = "API key verplicht.",
+                ProblemInstanceUri = _problemDetailsHelper.GetInstanceUri(_httpContextAccessor.HttpContext)
+            };
+    }
+}

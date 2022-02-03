8c01746a28fbe356848371a325bdf905ad6fae8e
Alain Thoen <Alain.Thoen@streamsoftware.be>
2022-02-03 15:20:18 +0100

    fix change to 406 comment

--- src/Be.Vlaanderen.Basisregisters.Api/Exceptions/NotAcceptableResponseExamples.cs
+++ src/Be.Vlaanderen.Basisregisters.Api/Exceptions/NotAcceptableResponseExamples.cs
@@ -7,7 +7,7 @@ namespace Be.Vlaanderen.Basisregisters.Api.Exceptions
     public class NotAcceptableResponseExamples : IExamplesProvider<ProblemDetails>
     {
         // https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.5
-        // The 304 response MUST NOT contain a message-body
+        // The 406 response MUST NOT contain a message-body
         public ProblemDetails GetExamples() => null;
     }
 }

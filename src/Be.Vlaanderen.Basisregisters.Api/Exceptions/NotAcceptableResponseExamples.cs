9d373fd47f7614ebe894ce23f77f558f35c8e4df
Arne Dumarey <dumarey.arne@gmail.com>
2022-02-04 15:41:54 +0100

    fix: commit 322ad

--- src/Be.Vlaanderen.Basisregisters.Api/Exceptions/NotAcceptableResponseExamples.cs
+++ src/Be.Vlaanderen.Basisregisters.Api/Exceptions/NotAcceptableResponseExamples.cs
@@ -1,13 +1,11 @@
 namespace Be.Vlaanderen.Basisregisters.Api.Exceptions
 {
-    using Microsoft.AspNetCore.Http;
     using Swashbuckle.AspNetCore.Filters;
-    using ProblemDetails = BasicApiProblem.ProblemDetails;
 
     public class NotAcceptableResponseExamples : IExamplesProvider<object>
     {
         // https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.5
         // The 406 response MUST NOT contain a message-body
-        public ProblemDetails GetExamples() => null;
+        public object GetExamples() => null;
     }
 }

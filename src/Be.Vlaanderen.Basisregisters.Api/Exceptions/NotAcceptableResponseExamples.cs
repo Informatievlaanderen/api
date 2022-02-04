322ad02406568537969a9d5632f9a84659afcb44
Arne Dumarey <dumarey.arne@gmail.com>
2022-02-04 15:33:25 +0100

    fix: `NotAcceptableResponseExamples` uses correct provider

--- src/Be.Vlaanderen.Basisregisters.Api/Exceptions/NotAcceptableResponseExamples.cs
+++ src/Be.Vlaanderen.Basisregisters.Api/Exceptions/NotAcceptableResponseExamples.cs
@@ -4,7 +4,7 @@ namespace Be.Vlaanderen.Basisregisters.Api.Exceptions
     using Swashbuckle.AspNetCore.Filters;
     using ProblemDetails = BasicApiProblem.ProblemDetails;
 
-    public class NotAcceptableResponseExamples : IExamplesProvider<ProblemDetails>
+    public class NotAcceptableResponseExamples : IExamplesProvider<object>
     {
         // https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html#sec10.3.5
         // The 406 response MUST NOT contain a message-body

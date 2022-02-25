ace3b6d0e20184b9155e2e85f970d0d644709a77
Arne Dumarey <dumarey.arne@gmail.com>
2022-02-25 10:01:02 +0100

    fix: remove unneeded header name

--- src/Be.Vlaanderen.Basisregisters.Api/CustomHeaderNames.cs
+++ src/Be.Vlaanderen.Basisregisters.Api/CustomHeaderNames.cs
@@ -3,6 +3,5 @@ namespace Be.Vlaanderen.Basisregisters.Api
     public static class CustomHeaderNames
     {
         public const string CommandId = "CommandId";
-        public const string LastObservedPosition = Api.LastObservedPosition.LastObservedPosition.HeaderName;
     }
 }

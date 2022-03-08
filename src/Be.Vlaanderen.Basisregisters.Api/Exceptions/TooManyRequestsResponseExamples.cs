9c8242c36f844b9c0a5f8ea1ed52d2c2c71f9191
pgallik <patrickgallik@gmail.com>
2022-03-08 14:39:42 +0100

    add TooManyRequestsResponseExamples

---
+++ src/Be.Vlaanderen.Basisregisters.Api/Exceptions/TooManyRequestsResponseExamples.cs
@@ -0,0 +1,30 @@
+namespace Be.Vlaanderen.Basisregisters.Api.Exceptions
+{
+    using BasicApiProblem;
+    using Microsoft.AspNetCore.Http;
+    using Swashbuckle.AspNetCore.Filters;
+
+    public class TooManyRequestsResponseExamples : IExamplesProvider<ProblemDetails>
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ProblemDetailsHelper _problemDetailsHelper;
+
+        public TooManyRequestsResponseExamples(
+            IHttpContextAccessor httpContextAccessor,
+            ProblemDetailsHelper problemDetailsHelper)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _problemDetailsHelper = problemDetailsHelper;
+        }
+
+        public ProblemDetails GetExamples() =>
+            new ProblemDetails
+            {
+                ProblemTypeUri = "urn:be.vlaanderen.basisregisters.api:throttled",
+                HttpStatus = StatusCodes.Status429TooManyRequests,
+                Title = ProblemDetails.DefaultTitle,
+                Detail = "U voert teveel requests uit in een korte tijdspanne. Probeer later opnieuw.",
+                ProblemInstanceUri = _problemDetailsHelper.GetInstanceUri(_httpContextAccessor.HttpContext)
+            };
+    }
+}

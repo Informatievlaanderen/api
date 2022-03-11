e058eb0b36c6044cbfe7d22991ec3f20347d3564
Arne Dumarey <dumarey.arne@gmail.com>
2022-03-11 08:29:29 +0100

    feat: add NoContentWithETagResult

---
+++ src/Be.Vlaanderen.Basisregisters.Api/ETag/NoContentWithETagResult.cs
@@ -0,0 +1,49 @@
+namespace Be.Vlaanderen.Basisregisters.Api.ETag
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Net.Http.Headers;
+
+    public class NoContentWithETagResult : NoContentResult
+    {
+        public string ETag { get; }
+
+        public NoContentWithETagResult(string eTag)
+        {
+            ETag = eTag;
+        }
+
+        public override async Task ExecuteResultAsync(ActionContext context)
+        {
+            AddLastObservedPositionAsEtag(context);
+            await base.ExecuteResultAsync(context);
+        }
+
+        public override void ExecuteResult(ActionContext context)
+        {
+            AddLastObservedPositionAsEtag(context);
+            base.ExecuteResult(context);
+        }
+
+        private void AddLastObservedPositionAsEtag(ActionContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var etag = new ETag(ETagType.Strong, ETag);
+
+            if (context.HttpContext.Response.Headers.Keys.Contains(HeaderNames.ETag))
+            {
+                context.HttpContext.Response.Headers[HeaderNames.ETag] = etag.ToString();
+            }
+            else
+            {
+                context.HttpContext.Response.Headers.Add(HeaderNames.ETag, etag.ToString());
+            }
+        }
+    }
+}

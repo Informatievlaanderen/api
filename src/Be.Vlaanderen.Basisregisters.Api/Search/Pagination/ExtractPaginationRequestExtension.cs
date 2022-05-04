1aadc7fd67e09cb47ee3fe7e95ced73e488d67d3
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-05-04 11:33:56 +0200

    fix: fix pagination extensions

--- src/Be.Vlaanderen.Basisregisters.Api/Search/Pagination/ExtractPaginationRequestExtension.cs
+++ src/Be.Vlaanderen.Basisregisters.Api/Search/Pagination/ExtractPaginationRequestExtension.cs
@@ -11,23 +11,34 @@ namespace Be.Vlaanderen.Basisregisters.Api.Search.Pagination
         public static IPaginationRequest ExtractPaginationRequest(
             this HttpRequest request,
             int defaultOffset = 0,
-            int defaultLimit = 100, 
+            int defaultLimit = 100,
             int maxLimit = 500)
         {
             var pagination = request.Headers[AddPaginationExtension.HeaderName];
 
             if (pagination == new StringValues(NoPagination))
+            {
                 return new NoPaginationRequest();
+            }
 
             var offset = defaultOffset;
             var limit = defaultLimit;
 
-            if (string.IsNullOrEmpty(pagination))
-                return new PaginationRequest(offset, limit);
+            if (int.TryParse(request.Query["offset"], out var newOffset))
+            {
+                offset = newOffset;
+            }
 
-            var headerValues = pagination.ToString().Split(new [] { ','}, 2, StringSplitOptions.RemoveEmptyEntries);
-            int.TryParse(headerValues[0], out offset);
-            int.TryParse(headerValues[1], out limit);
+            if (int.TryParse(request.Query["limit"], out var newLimit))
+            {
+                limit = newLimit;
+                limit = Math.Min(limit, maxLimit);
+            }
+
+            if (string.IsNullOrEmpty(pagination))
+            {
+                return new PaginationRequest(offset, limit);
+            }
 
             limit = Math.Min(limit, maxLimit);
 

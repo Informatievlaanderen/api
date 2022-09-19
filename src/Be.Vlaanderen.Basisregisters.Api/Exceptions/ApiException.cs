e37060b9d21eee1a9b85abfd421e3c7991f1f8c9
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-09-19 15:25:23 +0200

    fix: make exceptions serializable

--- src/Be.Vlaanderen.Basisregisters.Api/Exceptions/ApiException.cs
+++ src/Be.Vlaanderen.Basisregisters.Api/Exceptions/ApiException.cs
@@ -1,20 +1,36 @@
 namespace Be.Vlaanderen.Basisregisters.Api.Exceptions
 {
     using System;
+    using System.Runtime.Serialization;
     using Microsoft.AspNetCore.Http;
 
+    /// <inheritdoc />
+    [Serializable]
     public class ApiException : Exception
     {
+        /// <summary>
+        /// HTTP status code.
+        /// </summary>
         public int StatusCode { get; } = StatusCodes.Status500InternalServerError;
 
+        /// <inheritdoc />
         public ApiException() { }
 
+        /// <inheritdoc />
         public ApiException(string message) : base(message)  { }
 
+        /// <inheritdoc />
         public ApiException(string message, Exception inner) : base(message, inner) { }
 
+        /// <inheritdoc />
         public ApiException(string message, int statusCode) : base(message) => StatusCode = statusCode;
 
+        /// <inheritdoc />
         public ApiException(string message, int statusCode, Exception inner) : base(message, inner) => StatusCode = statusCode;
+
+        /// <inheritdoc />
+        protected ApiException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        { }
     }
 }

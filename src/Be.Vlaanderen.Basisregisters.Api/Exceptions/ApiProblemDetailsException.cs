472a334b6d98e8a9f9d563c46dc346b4258f1a0b
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-09-19 15:25:23 +0200

    fix: make exceptions serializable

--- src/Be.Vlaanderen.Basisregisters.Api/Exceptions/ApiProblemDetailsException.cs
+++ src/Be.Vlaanderen.Basisregisters.Api/Exceptions/ApiProblemDetailsException.cs
@@ -1,16 +1,25 @@
 namespace Be.Vlaanderen.Basisregisters.Api.Exceptions
 {
     using System;
+    using System.Runtime.Serialization;
     using ProblemDetails = BasicApiProblem.ProblemDetails;
 
+    /// <inheritdoc />
+    [Serializable]
     public class ApiProblemDetailsException : ApiException
     {
         public ProblemDetails Details { get; }
 
+        /// <inheritdoc />
         public ApiProblemDetailsException(string message, int statusCode, ProblemDetails problemDetails, Exception innerException)
             : base(message, statusCode, innerException)
         {
             Details = problemDetails ?? throw new ArgumentNullException(nameof(problemDetails));
         }
+
+        /// <inheritdoc />
+        protected ApiProblemDetailsException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        { }
     }
 }

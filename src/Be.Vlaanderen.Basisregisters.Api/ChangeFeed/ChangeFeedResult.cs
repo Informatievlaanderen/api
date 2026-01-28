4e2311797f4018dbaa3e6e3b17c5d012d0434489
Arne Dumarey <dumarey.arne@gmail.com>
2026-01-28 14:00:43 +0100

    feat: add changefeedresult

---
+++ src/Be.Vlaanderen.Basisregisters.Api/ChangeFeed/ChangeFeedResult.cs
@@ -0,0 +1,42 @@
+﻿namespace Be.Vlaanderen.Basisregisters.Api.ChangeFeed;
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+public sealed class ChangeFeedResult : ContentResult
+{
+    public const string PageCompleteHeaderName = "X-Page-Complete";
+
+    private readonly bool _isComplete;
+
+    public ChangeFeedResult(string content, bool isComplete)
+    {
+        StatusCode = StatusCodes.Status200OK;
+        ContentType = AcceptTypes.JsonCloudEventsBatch;
+        Content = content;
+
+        _isComplete = isComplete;
+    }
+
+    public override async Task ExecuteResultAsync(ActionContext context)
+    {
+        AddIsCompleteHeader(context);
+        await base.ExecuteResultAsync(context);
+    }
+
+    public override void ExecuteResult(ActionContext context)
+    {
+        AddIsCompleteHeader(context);
+        base.ExecuteResult(context);
+    }
+
+    private void AddIsCompleteHeader(ActionContext context)
+    {
+        if(context == null)
+            throw new ArgumentNullException(nameof(context));
+
+        context.HttpContext.Response.Headers.Append(PageCompleteHeaderName, _isComplete.ToString());
+    }
+}
